937cb134ba7ae580a7cc757dc40587cf4f7b8d7d
TFullerene <s1trojacek@yahoo.com>
2017-02-23 18:31:58 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
   <div id="footer">
 	
 	TAMUC Student & Alumni Outreach Website
+	  This website is licensed under the GNU General Public License Version 3.
+	  For more information, please see: https://www.gnu.org/licenses/gpl.html
 	
 	
     </div>		
